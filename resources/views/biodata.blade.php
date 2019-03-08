334a931d17e029d51971bf02b60f233b76d58bde
Tiadeliamaydina <tiadeliabcd@gmail.com>
2019-03-09 06:04:17 +0700

    puss kesekian

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel #4 : Passing Data Controller Ke View Laravel - www.harongoding.com</title>
+</head>
+<body>
+ 
+	<h1>Tutorial Laravel</h1>
+	<a href="https://www.harongoding.com/category/laravel">www.harongoding.com</a>
+ 
+</body>
+</html>
